2c6bf25827e7410ba732e9d45e7f5b187f5f5c3e
Cristiano Sancho <cristiano.sancho@4strats.com>
2024-07-10 19:59:21 -0300

    new files backend

--- terraform/s3-bucket-static/.backend.tf
+++ terraform/s3-bucket-static/.backend.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.56.0"
+      version = "5.56.0"
     }
-    
+  }
   backend "s3" {
     bucket = "sancho-terraform-state"
     key    = "state/aws/sdlf/sdlf-cicd/terraform.tfstate"
