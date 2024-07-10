830f1e8e94438998fe0fdd21abb0dd79bad2bf44
Cristiano Sancho <cristiano.sancho@4strats.com>
2024-07-10 18:29:01 -0300

    change file

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -1,43 +1,47 @@
+
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"
 }
 
 variable "bucket_name" {
-  type = string
+    type = string
 }
 
 resource "aws_s3_bucket" "static_site_bucket" {
     bucket = "static-site-${var.bucket_name}"
 
-    tags = {
-      Name = "Static Site"
-      Env = "Prod"
+    website {
+        index_document = "index.html"
+        error_document = "404.html"
     }
 
+    tags = {
+        Name = "Static Site Bucket"
+        Environment = "Production"
+    }
 }
 
-resource "aw3_s3_bucket_public_access_block" "static_site_bucket" {
-  bucket = aws_s3_bucket.static_site_bucket.id
+resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
+    bucket = aws_s3_bucket.static_site_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
-
   rule {
     object_ownership = "BucketOwnerPreferred"
-  }  
+  }
 }
-    
+
 resource "aws_s3_bucket_acl" "static_site_bucket" {
   depends_on = [
-    aw3_s3_bucket_public_access_block.static_site_bucket,
-    aws_s3_bucket_ownership_controls.static_site_bucket,
-    ]
+	aws_s3_bucket_public_access_block.static_site_bucket,
+	aws_s3_bucket_ownership_controls.static_site_bucket,
+  ]
 
   bucket = aws_s3_bucket.static_site_bucket.id
   acl    = "public-read"
